54dd7b2c1b8836383e1d7bb2a0a3f41052681a4f
Samuel Agbonkpolo <samuelagm@gmail.com>
2025-07-06 22:26:06 +0100

    style: update health check response message
    - Changed health check response from "Alive and Well" to "Alive and Well 1"

--- internal/server/server.go
+++ internal/server/server.go
@@ -26,7 +26,7 @@ func Listen(ctx context.Context, dbClient *ent.Client) {
 	})
 
 	r.GET("/health", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "Alive and Well")
+		ctx.String(http.StatusOK, "Alive and Well 1")
 	})
 
 	v1 := r.Group("/api/v1")
