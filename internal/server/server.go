060601e11a795463861c5ce5ed5a02f28040b051
Samuel Agbonkpolo <samuelagm@gmail.com>
2025-07-09 14:24:39 +0100

    style: update health check response message to reflect current status

--- internal/server/server.go
+++ internal/server/server.go
@@ -26,7 +26,7 @@ func Listen(ctx context.Context, dbClient *ent.Client) {
 	})
 
 	r.GET("/health", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "Alive and Well 6")
+		ctx.String(http.StatusOK, "Alive and Well 7")
 	})
 
 	v1 := r.Group("/api/v1")
