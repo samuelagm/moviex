a258a57de03480cf6eee49d22f0ca024989e5802
Samuel Agbonkpolo <samuelagm@gmail.com>
2025-12-20 21:16:43 +0100

    chore

--- internal/server/server.go
+++ internal/server/server.go
@@ -26,7 +26,7 @@ func Listen(ctx context.Context, dbClient *ent.Client) {
 	})
 
 	r.GET("/health", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "Alive and Well 15")
+		ctx.String(http.StatusOK, "Alive and Well 16")
 	})
 
 	v1 := r.Group("/api/v1")
