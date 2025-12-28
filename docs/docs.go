dfca5d95a7ac9e24ec8c0af43a7d3c3dd5a78628
Samuel Agbonkpolo <samuelagm@gmail.com>
2025-12-28 16:51:46 +0100

    Update swagger docs to version 0.0.3

--- docs/docs.go
+++ docs/docs.go
@@ -440,7 +440,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.0.2",
+	Version:          "0.0.3",
 	Host:             "moviex.fly.dev",
 	BasePath:         "",
 	Schemes:          []string{},
