789b0a70d0aecaa6db3cac9b4099878a1b62bd20
Samuel Agbonkpolo <samuelagm@gmail.com>
2026-01-24 23:30:43 +0100

    style: update health message format in server response

--- internal/server/server.go
+++ internal/server/server.go
@@ -27,7 +27,7 @@ func Listen(ctx context.Context, dbClient *ent.Client) {
 	env1 := os.Getenv("ENV_1")
 	sec1 := os.Getenv("SEC_1")
 	log.Printf("ENV_1=%s SEC_1=%s", env1, sec1)
-	healthMessage := fmt.Sprintf("Alive and Well 24 | ENV_1=%s SEC_1=%s", env1, sec1)
+	healthMessage := fmt.Sprintf("Alive and Well 25 | ENV_1=%s SEC_1=%s", env1, sec1)
 
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, fmt.Sprintf("Welcome, see: /api/v1/docs/index.html. %s", healthMessage))
